b3369f0e4df581ac8951ae25ed5f43bb22d06791
Sonjasusan <sonja.nyman92@gmail.com>
2022-06-28 13:17:35 +0300

    Healthbar

--- Omapeli/Assets/Scripts/PlayerBehavior.cs
+++ Omapeli/Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
+    [SerializeField] HealthBar  _healthbar;
 
     void Start()
     {
@@ -28,12 +29,15 @@ public class PlayerBehavior : MonoBehaviour
     private void PlayerTakeDmg(int dmg)
     {
         GameManager.gameManager._playerHealth.dmgUnit(dmg);
+        _healthbar.SetHealth(GameManager.gameManager._playerHealth.PlayerHealth);
 
     }
 
     private void PlayerHeal(int healing)
     {
         GameManager.gameManager._playerHealth.healUnit(healing);
+        _healthbar.SetHealth(GameManager.gameManager._playerHealth.PlayerHealth);
+
 
     }
 }
